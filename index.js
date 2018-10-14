5b67f64ed7cdec3b1f49bd0464da56a6c904502b
Tyler Devin <tylerddavis@rocketmail.com>
2018-10-14 20:31:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
-let randomUserID = Math.floor(Math.random() * Math.floor(max));
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+let randomUserID = getRandomInt(1000000020));
